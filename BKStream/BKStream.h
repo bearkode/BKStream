05cc867b064d126fde652f5811d0ac497ab29c2d
김창기/앱기술개발팀 <cgkim@nhnent.com>
2015-04-07 17:24:36 +0900

    Initial Commit

---
+++ BKStream/BKStream.h
@@ -0,0 +1,13 @@
+//
+//  BKStream.h
+//  BKStream
+//
+//  Created by cgkim on 2015. 4. 7..
+//  Copyright (c) 2015년 bearkode. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BKStream : NSObject
+
+@end
---
+++ BKStream/BKStream.m
@@ -0,0 +1,13 @@
+//
+//  BKStream.m
+//  BKStream
+//
+//  Created by cgkim on 2015. 4. 7..
+//  Copyright (c) 2015년 bearkode. All rights reserved.
+//
+
+#import "BKStream.h"
+
+@implementation BKStream
+
+@end
